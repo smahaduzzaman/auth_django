74395a3ed87f50dfa87fd2ed9250bc3a74e83ea8
S M Ahaduzzaman <smahaduzzaman96@gmail.com>
2024-03-31 23:44:06 +0600

    User can register with the credientials

--- authproject/authapp/views.py
+++ authproject/authapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 @login_required
@@ -25,8 +27,21 @@ def login(request):
         return render(request, 'authapp/login.html')
     
 def register(request):
-    return render(request, 'authapp/register.html')
+    username= request.POST.get('username')
+    first_name= request.POST.get('first_name')
+    last_name= request.POST.get('last_name')
+    email= request.POST.get('email')
+    password1= request.POST.get('password1')
+    password2= request.POST.get('password2')
+    
+    print(username, first_name, last_name, email, password1, password2)
+    
+    user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password1)
+    user.save()
+    messages.success(request, 'User created successfully')
 
+    return render(request, 'authapp/register.html')
+        
 def logout(request):
     auth_logout(request)
-    return render(request, 'authapp/index.html')
+    return render(request, 'authapp/login.html')
